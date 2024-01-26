9155b01c6dccbe01361d583259759a0e81cce3a0
AnkaBagishvili <lukiana.3@gmail.com>
2024-01-26 08:44:50 -0800

    Task2

---
+++ Lesson3Homework/script.js
@@ -0,0 +1,52 @@
+// let expo = prompt("Enter a number"); 
+// alert(expo ** 2);
+
+// let temp = prompt("Enter Degree Celsius");
+// alert((temp * 9 / 5) + 32 + "°F");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let chooseYear = prompt("Enter year");
+if (chooseYear % 4 == 0) {
+    alert("მოცემული წელი არ არის ნაკიანი");
+}
+else {
+    alert("მოცემული წელი ნაკიანია");
+}
+
+// let num1 = parseFloat(prompt("Enetr number 1"));
+// let num2 = parseFloat(prompt("Enetr number 2"));
+// let oper = prompt("Enter math operation (+ ; - ; * ; / ; ** ;)");
+// function calculate(num1, num2) {
+//     switch (oper) {
+//         case "+":
+//             return num1 + num2;
+
+//         case "-":
+//             return num1 - num2;
+
+//         case "*":
+//             return num1 * num2;
+
+//         case "/":
+//             return num1 / num2;
+
+//         case "**":
+//             return num1 ** num2;
+//     }
+// }
+// alert(calculate(num1, num2));
+
+
+
