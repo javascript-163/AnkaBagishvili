843ca8c6f9ae26c449a95a4c90b48eb37b60914b
AnkaBagishvili <lukiana.3@gmail.com>
2024-02-16 02:07:35 -0800

    Task 2

---
+++ Lesson9Homework/Images/scripts/images.js
@@ -0,0 +1,17 @@
+// 2. შექმენით საიტი სადაც მომხმარებელს შეეძლება აირჩიოს მისთვის სასურველი სურათი, შემდეგში კი გამოიტანეთ ეს სურათი ეკრანზე.
+function getImage() {
+  return new Promise(() => {
+    let imageUpload = document.getElementById(`imageUpload`);
+    let imageContent = document.getElementById("imageContent");
+    console.log(imageUpload.files[0]);
+    let file = imageUpload.files[0];
+    if (file) {
+      let reader = new FileReader();
+      reader.onload = function (event) {
+        imageContent.src = event.target.result;
+        imageContent.style.display = "block";
+      };
+      reader.readAsDataURL(imageUpload.files[0]);
+    }
+  });
+};
